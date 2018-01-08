cdbe1dc55ec748374d4d49ea41d130818b05c509
tsuji <>
2018-01-08 18:01:57 +0900

    add new and form

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -3,4 +3,19 @@ class PlaysController < ApplicationController
   def index
   end
   
+
+  def new
+    @play = Play.new
+  end
+
+  def create
+    @play = Play.new(play_params)
+  end
+
+  private
+
+  def play_params
+    params.require(:play).permit(:title, :descript, :director)
+  end
+
 end
